f57504e8b9b53acea5c55f106bef4214292b2f6f
Tomgast <Tom@degruijterweb.nl>
2025-08-08 12:40:32 +0200

    qwe

--- final-delete-user.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config/secure_loader.php';
-require_once 'includes/database_helper.php';
-
-$pdo = DatabaseHelper::getConnection();
-$stmt = $pdo->prepare('DELETE FROM users WHERE id = 9');
-$stmt->execute();
-echo 'User 9 deleted: ' . $stmt->rowCount() . ' records';
-?>
